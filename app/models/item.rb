face77b61bf5e36e608c4a4594eb6ff4a4195dd3
yusukeooy <mi6codenooo7@gmail.com>
2023-10-11 16:25:20 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,26 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :sales_status
+  belongs_to :category
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_info
+    validates :price, numericality: { only_integer: true, in: 300..9_999_999 }
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :sales_status_id 
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         has_many :items
+
          with_options presence: true do
           validates :nickname
           validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
