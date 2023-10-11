d8458296c5562f3da9d4b28ef87a5fe4d7748bea
yusukeooy <mi6codenooo7@gmail.com>
2023-10-11 19:17:49 +0900

    モデル単体テストコードの実装 修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :item do
-    name                      { Faker::Commerce }
-    explanation               { Faker::Lorem.sentence }
+    item_name                 { Faker::Commerce }
+    item_info                 { Faker::Lorem.sentence }
     category_id               { Faker::Number.between(from: 2, to: 11) }
-    condition_id              { Faker::Number.between(from: 2, to: 7) }
-    derivery_cost_id          { Faker::Number.between(from: 2, to: 3) }
+    sales_status_id           { Faker::Number.between(from: 2, to: 7) }
+    shipping_fee_status_id    { Faker::Number.between(from: 2, to: 3) }
     prefecture_id             { Faker::Number.between(from: 2, to: 48) }
-    sending_date_id           { Faker::Number.between(from: 2, to: 4) }
+    scheduled_delivery_id     { Faker::Number.between(from: 2, to: 4) }
     price                     { Faker::Number.between(from: 300, to: 9_999_999) }
     association :user
     after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,77 +6,77 @@ RSpec.describe Item, type: :model do
   end
   describe 'itemの出品' do
     context 'itemが出品できる場合' do
-      it ‘商品画像、商品名、商品の説明、カテゴリー、商品の状態、配送料の負担、発送元の地域、発送までの日数、価格を入力すれば出品できる’ do
+      it '商品画像、商品名、商品の説明、カテゴリー、商品の状態、配送料の負担、発送元の地域、発送までの日数、価格を入力すれば出品できる' do
         expect(@item).to be_valid
       end
     end
     context 'itemが出品できない場合' do
-      it ‘ユーザー情報が紐づいていないと出品できない’ do
+      it 'ユーザー情報が紐づいていないと出品できない' do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('User must exist')
       end
-      it ‘画像が空では出品できない’ do
+      it '画像が空では出品できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it ‘商品名が空では出品できない’ do
-        @item.name = ''
+      it '商品名が空では出品できない' do
+        @item.item_name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Name can't be blank")
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
-      it ‘商品の説明が空では出品できない’ do
-        @item.explanation = ''
+      it '商品の説明が空では出品できない' do
+        @item.item_info = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+        expect(@item.errors.full_messages).to include("Item info can't be blank")
       end
-      it ‘カテゴリーが---では出品できない’ do
+      it 'カテゴリーが---では出品できない' do
         @item.category_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it ‘商品の状態が---では出品できない’ do
-        @item.condition_id = '1'
+      it '商品の状態が---では出品できない' do
+        @item.sales_status_id  = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition can't be blank")
+        expect(@item.errors.full_messages).to include("Sales status can't be blank")
       end
-      it ‘配達料の負担が---では出品できない’ do
-        @item.derivery_cost_id = '1'
+      it '配達料の負担が---では出品できない' do
+        @item.shipping_fee_status_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Derivery cost can't be blank")
+        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
       end
-      it ‘発送元の地域が---では出品できない’ do
+      it '発送元の地域が---では出品できない' do
         @item.prefecture_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
-      it ‘発送までの日数が---では出品できない’ do
-        @item.sending_date_id = '1'
+      it '発送までの日数が---では出品できない' do
+        @item. scheduled_delivery_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Sending date can't be blank")
+        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
       end
-      it ‘価格が空では出品できない’ do
+      it '価格が空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it ‘価格が全角数字を含むと出品できない’ do
+      it '価格が全角数字を含むと出品できない' do
         @item.price = '１１１000'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
-      it ‘価格が英字では出品できない’ do
+      it '価格が英字では出品できない' do
         @item.price = 'aaaaa'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
-      it ‘価格が300円未満では出品できない’ do
+      it '価格が300円未満では出品できない' do
         @item.price = '299'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be in 300..9999999')
       end
-      it ‘価格が10,000,000円以上では出品できない’ do
+      it '価格が10,000,000円以上では出品できない' do
         @item.price = '10000000'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be in 300..9999999')
