4083a054f3cb0f880ac80c414767710df8beba16
yusukeooy <mi6codenooo7@gmail.com>
2023-10-12 21:21:10 +0900

    Revert "商品削除機能の実装"
    This reverts commit 46bb85bc737c106c8505b26b83707e99a0f468c5.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :load_item, only: [:show, :edit, :update, :destroy]
+  before_action :load_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -34,11 +34,6 @@ class ItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
-  end
- 
-  def destroy
-       @item.destroy
-       redirect_to root_path
   end
 
   private
