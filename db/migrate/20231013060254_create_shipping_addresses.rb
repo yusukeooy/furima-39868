462994920b254b6094289d3e7f242004e6348e6a
yusukeooy <mi6codenooo7@gmail.com>
2023-10-13 15:08:17 +0900

    各モデルの実装

---
+++ db/migrate/20231013060254_create_shipping_addresses.rb
@@ -0,0 +1,14 @@
+class CreateShippingAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shipping_addresses do |t|
+        t.string        :portal_code,         null: false, format: { with: /\A\d{3}-\d{4}\z/ }
+        t.integer       :prefecture_id,       null: false
+        t.string        :city,                null: false
+        t.string        :address,             null: false
+        t.string        :building
+        t.string        :phone,               null: false
+        t.references    :order,               null: false, foreign_key: true
+        t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_055952) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_13_060254) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_055952) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "addresses", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "item_name", null: false
@@ -52,6 +57,28 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_055952) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "orders", charset: "utf8", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "shipping_addresses", charset: "utf8", force: :cascade do |t|
+    t.string "portal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "phone", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_shipping_addresses_on_order_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -75,4 +102,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_055952) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
+  add_foreign_key "shipping_addresses", "orders"
 end
