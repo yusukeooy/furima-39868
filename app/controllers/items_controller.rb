2c650a58c8f921d064744ee78cb7a633f0e82058
yusukeooy <mi6codenooo7@gmail.com>
2023-10-17 20:51:55 +0900

    挙動確認後の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,8 +23,10 @@ class ItemsController < ApplicationController
   end
   
   def edit
-    return unless (current_user.id == @item.user_id) || @item.order.present?
-      redirect_to action: :index
+    if current_user.id == @item.user_id || @item.order.present? 
+       render :edit
+    else
+       redirect_to root_path
     end
   end
   
@@ -54,3 +56,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name,:item_info,:category_id,:sales_status_id,:shipping_fee_status_id,:prefecture_id,:scheduled_delivery_id,:price,:image).merge(user_id: current_user.id)
   end
+end
